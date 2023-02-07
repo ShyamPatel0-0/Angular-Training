dafd6221c8655c69548dcced9252370ab74910fc
ShyamPatel0-0 <120573342+ShyamPatel0-0@users.noreply.github.com>
2023-02-07 12:06:43 +0530

    1 binaries uploaded

--- Training/src/app/app.component.ts
+++ Training/src/app/app.component.ts
@@ -7,31 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Training';
-
-  users = [
-    {
-      name: 'Anil',
-      email: 'anil@test.com',
-      phone: '8888',
-      accounts: ['facebook', 'instagram', 'gmail'],
-    },
-    {
-      name: 'Pranav',
-      email: 'pranav@test.com',
-      phone: '1111',
-      accounts: ['youtube', 'instagram', 'gmail'],
-    },
-    {
-      name: 'Shyam',
-      email: 'shyam@test.com',
-      phone: '5555',
-      accounts: ['linkedIn', 'instagram', 'gmail'],
-    },
-    {
-      name: 'Raj',
-      email: 'raj@test.com',
-      phone: '6666',
-      accounts: ['Twitter', 'instagram', 'gmail'],
-    },
-  ];
+  
+  color = 'pink';
+  bgcolor = 'green';
+  updateColor() {
+    this.color = 'blue';
+    this.bgcolor = 'red';
+  }
 }
