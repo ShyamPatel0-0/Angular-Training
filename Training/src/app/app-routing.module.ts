f2cfd69bb4c830e5c6b4709f736fac0d900b4b45
ShyamPatel0-0 <120573342+ShyamPatel0-0@users.noreply.github.com>
2023-02-08 18:58:36 +0530

    simple routing

--- Training/src/app/app-routing.module.ts
+++ Training/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from '../../../Training/src/app/about/about.component';
+import { HomeComponent } from '../../../Training/src/app/home/home.component';
+import { UserComponent } from '../../../Training/src/app/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Training/src/app/directives/red-element.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RedElementDirective } from './red-element.directive';
-
-describe('RedElementDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RedElementDirective();
-    expect(directive).toBeTruthy();
-  });
-});
