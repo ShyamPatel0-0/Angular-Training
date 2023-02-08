d70437d955bdf5865e19d8ed91eefeaa28d3393f
ShyamPatel0-0 <120573342+ShyamPatel0-0@users.noreply.github.com>
2023-02-08 18:29:17 +0530

    custom directives

--- Training/src/app/app.component.ts
+++ Training/src/app/app.component.ts
@@ -9,25 +9,5 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent {
   title = 'Training';
 
-  loginForm = new FormGroup({
-    user: new FormControl('', [
-      Validators.required,
-      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(8),
-    ]),
-  });
-
-  loginUser() {
-    console.log(this.loginForm.value);
-  }
-
-  get user() {
-    return this.loginForm.get('user');
-  }
-  get password() {
-    return this.loginForm.get('password');
-  }
+ 
 }
--- Training/src/app/app.module.ts
+++ Training/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RedElementDirective } from './directives/red-element.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RedElementDirective],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
