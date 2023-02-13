5ba31227158569287f775e5f8f2d23d2a8ab35aa
Shyam Mendapara <shyamm@meditab.local>
2023-02-13 12:51:04 +0530

    dynamic routing by id

--- Training/src/app/user/user.component.ts
+++ Training/src/app/user/user.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+  userId:string | any ='';
+  constructor(private route: ActivatedRoute) {
 
+  }
+  ngOnInit(): void {
+    console.log(this.route.snapshot.paramMap.get('id'));
+    this.userId = this.route.snapshot.paramMap.get('id');
+  }
+  
 }
