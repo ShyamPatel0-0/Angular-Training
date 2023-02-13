bda8354ce08f702a5c6368331d24e07b172408f4
Shyam Mendapara <shyamm@meditab.local>
2023-02-13 13:12:19 +0530

    child routing

--- Training/src/app/app-routing.module.ts
+++ Training/src/app/app-routing.module.ts
@@ -3,10 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from '../../../Training/src/app/about/about.component';
 import { HomeComponent } from '../../../Training/src/app/home/home.component';
 import { UserComponent } from '../../../Training/src/app/user/user.component';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { NoPageComponent } from './no-page/no-page.component';
 
 const routes: Routes = [
   {
     path: 'about',
+    children:[
+      {
+        path:'company',
+        component:AboutCompanyComponent,
+      },
+      {
+        path:'me',
+        component:AboutMeComponent,
+      },
+    ],
     component: AboutComponent,
   },
   {
@@ -16,6 +29,10 @@ const routes: Routes = [
   {
     path: 'user/:id',
     component: UserComponent,
+  },
+  {
+    path: '**',
+    component: NoPageComponent,
   },
 ];
 
--- Training/src/app/app.module.ts
+++ Training/src/app/app.module.ts
@@ -7,9 +7,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { UserComponent } from './user/user.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { NoPageComponent } from './no-page/no-page.component';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, HomeComponent, AboutComponent],
+  declarations: [AppComponent, UserComponent, HomeComponent, AboutComponent, NoPageComponent, AboutCompanyComponent, AboutMeComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
